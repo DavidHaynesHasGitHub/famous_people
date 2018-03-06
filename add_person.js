db179401a7c493e55d31a4c98e113a24cc4bc5bc
David <daivdhayensvictoria@gmail.com>
2018-03-06 22:05:21 +0000

    added knex functionality for search and add

---
+++ add_person.js
@@ -0,0 +1,16 @@
+const settings = require("./settings"); // settings.json
+const knex = require("knex")({
+  client: 'pg',
+  connection: {
+   host     : settings.hostname,
+   user     : settings.user,
+   password : settings.password,
+   database : settings.database,
+  }
+});;
+
+knex('famous_people')
+  .returning('id')
+  .insert([{first_name: process.argv[2], last_name:process.argv[3], birthdate:process.argv[4]}]).asCallback(function (err, data) {
+    console.log('inserted data:', data);
+});
