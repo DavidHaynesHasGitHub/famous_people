e4358813e389fe5a15e864261f3a41c4f31add6d
David Haynes <DavidHaynesVictoria@gmail.com>
2018-03-06 14:21:51 -0800

    fixed formatting of the output string

--- search_script.js
+++ search_script.js
@@ -9,12 +9,13 @@ const knex = require("knex")({
   }
 });;
 
-
+console.log('Searching...');
 
 knex.select().from('famous_people')
   .where('first_name', process.argv[2])
   .orWhere('last_name', process.argv[2])
   .asCallback(function(err, rows) {
   if (err) return console.error(err);
-    console.log(rows);
+    console.log(`Found (${rows.length}) persons by the name \'${process.argv[2]}\':`);
+    console.log(JSON.stringify(rows))
   });
